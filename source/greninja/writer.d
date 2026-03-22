d6095edd8c73248454b1c627804ff381c9a00618
ankit <sahuankit.murhu@gmail.com>
2026-03-22 11:42:28 +0530

    feat: variable and rule writer

--- source/greninja/writer.d
+++ source/greninja/writer.d
@@ -2,7 +2,8 @@ module greninja.writer;
 
 import std.stdio;
 
-class Writer{
+class Writer
+{
   File output;
   uint width;
 
@@ -11,18 +12,51 @@ class Writer{
    1. name of the file
    2. width of the output, default is 78 
   **/
-  this(string filename, uint width = 78){
+  this(string filename, uint width = 78)
+  {
     this.output = File(filename, "w");
-    this.width = width; 
+    this.width = width;
   }
 
-  void line(string text, int indent = 0){
-    string leading_space = "  ";
-    foreach(_; 1 .. indent){
-      leading_space = "  " ~ leading_space;
+  /** 
+    * @params:
+    * 1. text to write
+    * 2. (optional) indent level default is 0
+  **/
+  void line(string text, int indent = 0)
+  {
+    string leading_space = "";
+    foreach (_; 0 .. indent)
+    {
+      leading_space ~= "  ";
     }
 
     this.output.writef("%s%s\n", leading_space, text);
   }
 
+  /** 
+   * @params:
+   * 1. name of the variable
+   * 2. value of variable
+   * 3. (optional) indent level default is 0
+   **/
+  void variable(string key, string value, int indent = 0)
+  {
+    import std.format : format;
+
+    if (value.length == 0)
+      return;
+    this.line(format("%s = %s", key, value), indent);
+  }
+
+  /** 
+    @params:
+    1. name of the rule
+    2. command to execute
+  **/
+  void rule(string name, string command)
+  {
+    this.line("rule " ~ name);
+    this.line("command = " ~ command, 1);
+  }
 }
